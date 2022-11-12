43559c978df1456e2c63bffa4097bb6f6dda2e24
Qun An Lin <qunan.lin04@htl-salzburg.ac.at>
2022-11-12 19:25:14 +0100

    init array1-4

--- Assets/MyGame/Scripts/ArrayUebung.cs
+++ Assets/MyGame/Scripts/ArrayUebung.cs
@@ -6,5 +6,19 @@ public class ArrayUebung : MonoBehaviour
 {
     int[] array;
 
+    int[] array1 = new int[4];
+    int[] array2 = new int[4] { 21, 22, 23, 24 };
+    int[] array3 = new int[7] { 31, 32, 33, 34, 35, 36, 37 };
+    string[] array4 = new string[10];
+
+
+    private void Start()
+    {
+        array4[0] = "array4";
+        GameObject[] arrayColoredImagesRed = GameObject.FindGameObjectsWithTag("RedImage");
+        Debug.Log(arrayColoredImagesRed[0].name);
+    }
+
+
 
 }
